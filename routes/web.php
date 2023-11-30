831ec46eb3b47f4cc713fe55354d596c87fe0eb4
Azaou123 <azawbadr@gmail.com>
2023-11-30 10:23:04 +0100

    basic back office setUp

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+
+
+// Home Controller : Navigate in the Front Office 
+    Route::get('/', function () {
+        return view('front.index');
+    });
+
+
+
+
+
+
+
+// Admin Controller : Navigate in the back Controller 
+    Route::get('/admin', function () {
+        return view('back.index');
+    });
+    Route::get('/students', function () {
+        return view('back.students');
+    });
+    Route::get('/teachers', function () {
+        return view('back.teachers');
+    });
+    Route::get('/exchanges', function () {
+        return view('back.exchanges');
+    });
+    Route::get('/workshops', function () {
+        return view('back.workshops');
+    });
+    Route::get('/projects', function () {
+        return view('back.projects');
+    });
+    Route::get('/fablabs', function () {
+        return view('back.teachers');
+    });
+    Route::get('/programs', function () {
+        return view('back.teachers');
+    });
+
